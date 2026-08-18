6df25f1c370e338af49e0191cdf77f6018308238
Mohammadreza Kaviyani <kiyarash17kaviyani@gmail.com>
2026-08-18 18:59:41 +0330

    header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+
+
+export default function Header() {
+  return (
+    <>
+      <div className="header">
+        <span className="header_tasklt">Taskly : </span>
+        <span>A simple and modern task management app</span>
+      </div>
+    </>
+  );
+}
